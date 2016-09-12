223704bc54729263660bdbfa35e5e52815c692a6
Jordan Lozinski <dogowner777@gmail.com>
2016-09-12 15:19:40 -0400

    began developing various features

--- replay.py
+++ replay.py
@@ -13,6 +13,7 @@
 #checking that the gamemode was sprint
 #	name.mode=LINE RACE
 #checking that the options are consistent.. e.g 40L vs 100L
+#	linerace.goaltype.-1:{0=20,1=40,2=100,3=10}
 #labeling axes
 
 #plotting variance:
@@ -23,8 +24,12 @@
 
 #making this code easier to modify
 
-
-
+"""
+GUI element features remaining to be implemented
+	More gamemodes
+	Max/min value plot
+	Variance
+"""
 import os, datetime
 import numpy as np
 import matplotlib.pyplot as plt
@@ -41,9 +46,7 @@ counter = 0
 oldContents = ""
 
 class Replay(object): #essentially used as a struct
-
-	gamemodedict = {"LINE RACE" : 0, "MARATHON" : 1}
-	def __init__(self, time, date, gamemode, num_frames, pieces, finesse, pps, filename, kpt):
+	def __init__(self, time, date, gamemode, num_frames, pieces, finesse, pps, filename, kpt, **gameargs):
 		self.frames = num_frames
 		self.pieces = pieces
 		self.gamemode = gamemode
@@ -52,15 +55,14 @@ class Replay(object): #essentially used as a struct
 		self.datetime = datetime.datetime.strptime(date.rstrip("\n")+"/"+"/".join(time.rstrip("\n").split("\:")),'%Y/%m/%d/%H/%M/%S')
 		self.filename = filename
 		self.kpt = kpt
+		if "sprinttype" in gameargs.keys():
+			self.lines = {0:20,1:40,2:100,3:10}[gameargs["goaltype"]]			
     
 def plot(*args):
 	global oldContents
 	global replays
-	varb = varbox.get()
-	while varb not in (1,2,3,4):
-		break
-		#send a mean error dialog
-	switch = {"PPS" : 1, "Time" : 2, "Finesse" : 3, "Pieces" : 4, "KPT" : 5}[varb]
+	varswitch = {"PPS" : 1, "Time" : 2, "Finesse" : 3, "Pieces" : 4, "KPT" : 5}[varbox.get()]
+	modeswitch = {"Line Race 40L" : 1, "Line Race 20L" : 1, "Line Race 10L" : 1, "Line Race 100L": 1, "Marathon" : 2}[gamemodesbox.get()]
 	if pathtext.get(1.0, tk.END) != oldContents: #if the filepaths have changed since we last populated the replays thing
 		replays = []
 		for c in replayFolders:
@@ -74,6 +76,7 @@ def plot(*args):
 				timeElapsed = ""
 				filename = replay
 				kpt = -1
+				kwargs = {}
 				replay = open(c+"\\"+replay)
 				for line in replay:
 					line = line.split("=")
@@ -87,14 +90,16 @@ def plot(*args):
 						pieces = int(line[1])
 					elif line[0] == "0.statistics.finesse":
 						finesse = int(line[1])
-					elif line[0] == "name.mode":
+					elif line[0] == "name.mode" and gamemode == "":
 						gamemode = line[1]
 					elif line[0] == "0.statistics.pps":
 						pps = float(line[1].strip('\n'))
 					elif line[0] == "0.statistics.kpt":
 						kpt = float(line[1])
-				if sprint and pieces > 100:
-					i = Replay(time, date, gamemode, num_frames, pieces, finesse, pps, filename, kpt)
+					elif line[0] == "linerace.goaltype.-1":
+						gamemode = "LINE RACE"
+						kwargs["goaltype"] = int(line[1])
+					i = Replay(time, date, gamemode, num_frames, pieces, finesse, pps, filename, kpt, kwargs)
 					replays.append(i)
 	else:
 		print("Detected no filepath change - using cached replayFolders")
@@ -102,15 +107,15 @@ def plot(*args):
 	oldContents = pathtext.get(1.0, tk.END)
 
 	dates = mdates.date2num([x.datetime for x in replays]) #if x.gamemode == (grab gamemode from gui)
-	if switch == 1:	
+	if varswitch == 1:	
 		values = [x.pps for x in replays]
-	elif switch == 2:
+	elif varswitch == 2:
 		values = [x.frames/60 for x in replays]
-	elif switch == 3:
+	elif varswitch == 3:
 		values = [x.finesse for x in replays]
-	elif switch == 4:
+	elif varswitch == 4:
 		values = [x.pieces for x in replays]
-	elif switch == 5:
+	elif varswitch == 5:
 		values = [x.kpt for x in replays]
 	plt.plot_date(dates, values)
 
@@ -151,7 +156,7 @@ varbox['values'] = ('PPS', 'Time', 'Finesse', 'Pieces', 'KPT')
 
 gamemodesbox = ttk.Combobox(mainframe, textvariable=gamevar, width=12)
 gamemodesbox.grid(column=0, row=3)
-gamemodesbox['values'] = ('Line Race 40L', 'Line Race 20L', 'Line Race 10L', 'Line Race 100L', 'Marathon')
+gamemodesbox['values'] = ('Line Race 40L', 'Line Race 20L', 'Line Race 100L', 'Line Race 10L', 'Marathon')
 
 maxvar = tk.BooleanVar()
 maxbutton = ttk.Checkbutton(mainframe, text='Plot max value', variable=maxvar)
